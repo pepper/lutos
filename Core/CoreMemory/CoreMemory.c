34467ddf4a5e7e0d9a41d32722b0edf4efcd5793
嚴 孝頤 <pepper@yan-xiaoyide-macbook-pro-84.local>
2009-09-23 00:38:38 +0800

    CoreMemory模組完成，但相關功能都還未詳盡測試！

--- Core/CoreMemory/CoreMemory.c
+++ Core/CoreMemory/CoreMemory.c
@@ -29,7 +29,9 @@ Data_Boolean CoreMemory_MemorySpaceVerify(void){
 }
 
 Data_Boolean CoreMemory_Defrag(void){
-	Data_1Byte i, j;
+	Data_Boolean needMove;
+	Data_1Byte i, j, newSpaceQuantity;
+	CoreMemory_Size spacing, newPhysicalMemoryUsedByte, k;
 	for(i = 0; i < CoreMemory_SpaceCurrentQuantity; i++){
 		if(CoreMemory_SpaceList[i].lock == FALSE || CoreMemory_SpaceList[i].used == FALSE){
 			CoreMemory_SpaceList[i].lock = TRUE;
@@ -44,11 +46,30 @@ Data_Boolean CoreMemory_Defrag(void){
 			return FALSE;
 		}
 	}
+	for(i = 0, newSpaceQuantity = 0, newPhysicalMemoryUsedByte = 0, needMove = FALSE, spacing = 0; i < CoreMemory_SpaceCurrentQuantity; i++){
+		if(CoreMemory_SpaceList[i].used == TRUE){
+			if(needMove == TRUE){
+				CoreMemory_SpaceList[newSpaceQuantity] = CoreMemory_SpaceList[i];
+				CoreMemory_SpaceList[newSpaceQuantity].startAddress -= spacing;
+				CoreMemory_SpaceList[i].used = FALSE;
+				for(k = 0; k < CoreMemory_SpaceList[newSpaceQuantity].size; k++){
+					CoreMemory_PhysicalMemorySpace[CoreMemory_SpaceList[newSpaceQuantity].startAddress + k] = CoreMemory_PhysicalMemorySpace[CoreMemory_SpaceList[i].startAddress + k];
+				}
+				spacing = 0;
+			}
+			newPhysicalMemoryUsedByte += CoreMemory_SpaceList[newSpaceQuantity].size;
+			newSpaceQuantity++;
+		}
+		else{
+			needMove = TRUE;
+			spacing += CoreMemory_SpaceList[i].size;
+		}
+	}
+	CoreMemory_PhysicalMemoryUsedByte = newPhysicalMemoryUsedByte;
+	CoreMemory_SpaceCurrentQuantity = newSpaceQuantity;
+	//Had bad efficiency, but more safety
 	for(i = 0; i < CoreMemory_SpaceCurrentQuantity; i++){
-		//Move Data
-		//Use usedSize++ count free space
-		//Change CoreMemory_SpaceCurrentQuantity
-		//Reverse CoreMemory_SpaceList[i].lock
+		CoreMemory_SpaceList[i].lock = FALSE;
 	}
 	return TRUE;
 }
@@ -74,8 +95,9 @@ Data_Boolean CoreMemory_DropMemory(void){
 		CoreMemory_PhysicalMemorySpace[k] = 0x00;
 	}
 	for(i = 0; i < CoreMemory_SpaceCurrentQuantity; i++){
-		CoreMemory_DeleteSpace(&CoreMemory_SpaceList[i]);
+		CoreMemory_SpaceList[i].used = FALSE;
 	}
+	CoreMemory_SpaceCurrentQuantity = 0;
 	return TRUE;
 }
 
@@ -88,7 +110,6 @@ CoreMemory_Space* CoreMemory_CreateSpace(Data_1Byte type, CoreMemory_Size size, 
 		space->startAddress = CoreMemory_PhysicalMemoryUsedByte;
 		space->size = size;
 		CoreMemory_PhysicalMemoryUsedByte += size;
-		space->endAddress = CoreMemory_PhysicalMemoryUsedByte;
 		space->elementSize = elementSize;
 		space->startPoint = 0;
 		space->endPoint = 0;
@@ -106,8 +127,6 @@ Data_Boolean CoreMemory_DeleteSpace(CoreMemory_Space* space){
 	if(space->lock == FALSE){
 		space->lock = TRUE;
 		space->used = FALSE;
-		CoreMemory_PhysicalMemoryUsedByte -= space->size;
-		CoreMemory_SpaceCurrentQuantity--;
 		return TRUE;
 	}
 	return FALSE;
@@ -115,10 +134,10 @@ Data_Boolean CoreMemory_DeleteSpace(CoreMemory_Space* space){
 
 Data_Boolean CoreMemory_DropSpace(CoreMemory_Space* space){
 	if(space->lock == FALSE){
-		CoreMemory_Size i;
+		CoreMemory_Size i, endAddress = space->startAddress + space->size;
 		space->lock = TRUE;
-		for(i = space->startAddress; i < space->endAddress; i++){
-			CoreMemory_PhysicalMemorySpace[i] = 0;
+		for(i = space->startAddress; i < endAddress; i++){
+			CoreMemory_PhysicalMemorySpace[i] = 0x00;
 		}
 		space->startPoint = 0;
 		space->endPoint = 0;
@@ -128,16 +147,6 @@ Data_Boolean CoreMemory_DropSpace(CoreMemory_Space* space){
 	}
 	return FALSE;
 }
-// INLINE void CoreMemory_DataWrite(Data_1Byte elementSize, void *target, void *data){
-// 	switch(elementSize){
-// 		case 1:
-// 		break;
-// 		case 2:
-// 		break;
-// 		case 4:
-// 		break;
-// 	}
-// }
 
 Data_Boolean CoreMemory_Push(CoreMemory_Space* space, CoreMemory_Size index, void *data){
 	if(space->lock == FALSE){
@@ -198,7 +207,17 @@ Data_Boolean CoreMemory_Push(CoreMemory_Space* space, CoreMemory_Size index, voi
 				break;
 			}
 			
-			case CoreMemory_SpaceTypeCustomType:{
+			case CoreMemory_SpaceTypeArray:{
+				if(index < (space->size / space->elementSize)){
+					dataStartAddress = space->startAddress + (index * space->elementSize);
+					for(i = 0; i < space->elementSize; i++){
+						CoreMemory_PhysicalMemorySpace[dataStartAddress + i] = inputData[i];
+					}
+				}
+				else{
+					space->lock = FALSE;
+					return FALSE;
+				}
 				break;
 			}
 		}
@@ -261,7 +280,16 @@ Data_Boolean CoreMemory_Pop(CoreMemory_Space* space, CoreMemory_Size index,void 
 			}
 			break;
 		}
-		case CoreMemory_SpaceTypeCustomType:{
+		case CoreMemory_SpaceTypeArray:{
+			if(index < (space->size / space->elementSize)){
+				dataStartAddress = space->startAddress + (index * space->elementSize);
+				for(i = 0; i < space->elementSize; i++){
+					outputData[i] = CoreMemory_PhysicalMemorySpace[dataStartAddress + i];
+				}
+			}
+			else{
+				return FALSE;
+			}
 			break;
 		}
 	}
--- Core/CoreMemory/CoreMemory.h
+++ Core/CoreMemory/CoreMemory.h
@@ -8,18 +8,18 @@
 #define	CoreMemory_SpaceTypeFIFO			0
 #define	CoreMemory_SpaceTypeCircularBuffer	1
 #define	CoreMemory_SpaceTypeStack			2
-#define	CoreMemory_SpaceTypeCustomType		3
+#define	CoreMemory_SpaceTypeArray			3
 
 #define	CoreMemory_EventHandlerTypeEmpty	0
 #define	CoreMemory_EventHandlerTypeFull		1
 #define	CoreMemory_EventHandlerTypeCross	2
 
+//建議當資料流遠大於22Byte才開始使用共用記憶體結構，因為22Byte是一個記憶體空間基本的Cost
 struct CoreMemory_SpaceMarker{
 	Data_1Byte			type;
 	Data_Boolean		used;
 	Data_Boolean		lock;
 	CoreMemory_Size		startAddress;
-	CoreMemory_Size		endAddress;
 	CoreMemory_Size		size;
 	Data_1Byte			elementSize;
 	CoreMemory_Size		startPoint;
@@ -33,7 +33,9 @@ void				CoreMemory_Init(void);
 Data_Boolean		CoreMemory_MemorySpaceVerify(void);
 Data_Boolean		CoreMemory_Defrag(void);
 Data_Boolean		CoreMemory_DropMemory(void);
+//Element Size也支援自定的Struct資料結構，只要利用sizeof(DataType)當做傳入參數即可
 CoreMemory_Space*	CoreMemory_CreateSpace(Data_1Byte type, CoreMemory_Size size, Data_1Byte elementSize);
+//就算DeleteMemory空間仍然沒有實際釋放出來給新的記憶體使用，直到執行Defrag才會真正將空間給釋放出來。
 Data_Boolean		CoreMemory_DeleteSpace(CoreMemory_Space* space);
 Data_Boolean		CoreMemory_DropSpace(CoreMemory_Space* space);
 Data_Boolean		CoreMemory_Push(CoreMemory_Space* space, CoreMemory_Size index, void *data);
